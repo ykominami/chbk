1f0bb577cd62f71bcce54cf054eab62e1d6a98f7
yasuo kominami <ykominami@gmail.com>
2016-07-13 13:29:39 +0900

    remove require of chbk/dbutil/chbmgr

--- lib/chbk/dbutil.rb
+++ lib/chbk/dbutil.rb
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 require 'chbk/dbutil/dbmgr'
-require 'chbk/dbutil/chbkmgr'
--- lib/chbk/dbutil/dbmgr.rb
+++ lib/chbk/dbutil/dbmgr.rb
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-require 'chbk/dbutil/chbkmgr'
 require 'forwardable'
 require 'pp'
 
