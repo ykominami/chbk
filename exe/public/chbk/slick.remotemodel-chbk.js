6d600759792927d1f0fdace8354e545e00fa81ef
yasuo kominami <ykominami@gmail.com>
2016-07-24 11:05:59 +0900

    implement addData method in RemoteModel

--- exe/public/chbk/slick.remotemodel-chbk.js
+++ exe/public/chbk/slick.remotemodel-chbk.js
@@ -9,10 +9,76 @@
 //	return Slick.Data.RemoteModel.call( this, [list_count_url , list_url] );
 //    };
 //    RemoteModel = inheritConstructor(Slick.Data.RemoteModelBase , function RemoteModel(){}, {});
-    
+
     // 正しい継承
     Slick.Data.inherits(RemoteModel, Slick.Data.RemoteModelBase);
-//    RM.prototype.constructor = RM;
+    //    RM.prototype.constructor = RM;
+
+    RemoteModel.prototype.addData = function addData( name , url , add_date ) {
+	console.log("addData");
+
+	if (this.category_id == null && this.path == null) {
+	    return;
+	}
+
+	if (this.req) {
+	    this.req.abort();
+	    this.data = undefined;
+	    /*
+	    this.data = { index: 0 };
+	    this.data[0].id = item.id
+	    this.data[0].name = item.name;
+	    this.data[0].url = item.url;
+	    */
+	}
+
+	var send_data = {
+	    name: name,
+	    url: encodeURIComponent( url ),
+	    add_date: add_date
+	}
+
+	if (this.category_id == null) {
+	    $.extend( send_data , { path: this.path } );
+	}
+	else{
+	    $.extend( send_data , { category_id: this.category_id } );
+	}
+
+	if (this.h_request != null) {
+	    clearTimeout(this.h_request);
+	}
+	var self = this;
+	this.h_request = setTimeout(function () {
+//	    this.data = {length: 0};
+	    if( ( "data" in self ) === false ){
+		self.data = { length: 0 }
+	    }
+	    else if( self.data === undefined ) {
+		self.data = { length: 0 }
+	    }
+	    self.data[0] = null; // null indicates a 'requested but not available yet'
+
+	    self.req = $.ajax({
+		data: send_data,
+//		type: "POST",
+		type: "GET",
+		url: self.add_item_url,
+		cache: true,
+		success: function (data, dataType) {
+		    // this;
+		    self.onSuccess(json, recStart)
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+		    self.onError(fromPage, toPage)
+		}
+	    });
+
+	}, 50);
+
+    }
+
+
     RemoteModel.prototype.onSuccess = function(json, recStart) {
 	var recEnd = recStart;
 	if (json.length > 0) {
@@ -24,7 +90,7 @@
 	    this.data.length = Math.min(recEnd,1000);
 	    for (var i = 0; i < results.length; i++) {
 		var item = results[i];
-		
+
 		this.data[recStart + i] = { index: recStart + i };
 		this.data[recStart + i].id = item.id
 		this.data[recStart + i].name = item.name;
@@ -35,11 +101,10 @@
 
 	this.onDataLoaded.notify({from: recStart, to: recEnd});
     }
-    
+
     RemoteModel.prototype.onError = function (fromPage, toPage) {
 	alert("Chbk:error loading pages " + fromPage + " to " + toPage);
     }
-
 
     $.extend(true, window, { Slick: { Data: { RemoteModel: RemoteModel }}});
 })(jQuery);
